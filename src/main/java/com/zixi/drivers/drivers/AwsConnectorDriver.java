c1afb591552b3ee7ffec963082fa681563b4419e
urasigal@gmail.com <urasigal@gmail.com>
2019-06-18 14:52:12 -0300

    fixes

--- src/main/java/com/zixi/drivers/drivers/AwsConnectorDriver.java
+++ src/main/java/com/zixi/drivers/drivers/AwsConnectorDriver.java
@@ -97,7 +97,7 @@ public class AwsConnectorDriver extends BroadcasterLoggableApiWorker
 															            if(  waiting / ( Integer.parseInt( operationParams.params.get("file_duration")) ) <= numOfUploadedFiles )
 															            	return new DriverReslut("The correct number of uploaded files was found");
 															            else  return new DriverReslut("Number of uploaded files is " + numOfUploadedFiles + " but should be "
-															            		+  waiting / ( Integer.parseInt( operationParams.params.get("file_duration") + " at least" )));
+															            		+  (waiting / ( Integer.parseInt( operationParams.params.get("file_duration")) + " at least" )));
 		        
 		        default: 											 return new DriverReslut("Operation is not found");
 	        }
